286fc479cff1584992d2ce872064084617dd58e0
Danila <ddanilsachko@gmail.com>
2024-02-19 18:45:51 +0300

    ended

--- lesson42Basic/main.cpp
+++ lesson42Basic/main.cpp
@@ -2,6 +2,43 @@
 
 using namespace std;
 
+void init(int* pointer, int size) {
+	cout << "Input array elements : ";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> *(pointer + i);
+	}
+}
+
+void init_second(int* pointer, int size) {
+	cout << "Input array elements : ";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> pointer[i];
+	}
+}
+
+void print(int* pointer, int size) {
+	cout << "Array elements : ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(pointer + i) << " ";
+	}
+}
+
+int get_max(int* pointer, int size) {
+
+	int max = *pointer;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (pointer[i] > max) {
+			max = pointer[i];
+		}
+	}
+	return max;
+}
+
 int main() {
 
 	int* pNum = new int;
@@ -10,6 +47,28 @@ int main() {
 	cout << pNum << " is address to " << *pNum;
 
 	delete pNum;
+
+	int size;
+
+	do {
+		cout << "\nInput size of array: ";
+		cin >> size;
+	} while (size <= 0);
+
+	int* pArray = new int[size];
+
+	/**pArray = 10;
+	*(pArray + 1) = 20;
+	*(pArray + 2) = 30;
+	*(pArray + 3) = 40;
+	*(pArray + 4) = 50;*/
+
+	init_second(pArray, size);
+	print(pArray, size);
+
+	cout << "\nMax number is " << get_max(pArray, size);
+
+	delete[] pArray;
 
 	return 0;
 }
